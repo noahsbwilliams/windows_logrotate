6bb5702864e7ef2698f82be1391336c7741e8a8e
Noah Williams <nwilliams@zixcorp.com>
2021-12-06 09:09:29 -0900

    Change the SourceForge download link syntax

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['windows_logrotate']['version'] = '0.0.0.18_20170228'
-default['windows_logrotate']['sha256'] = '11ca344f67c60905935e3037021b2229d6eb76c629724bf2cbab43713e101fd7'
+default['windows_logrotate']['sha256'] = '186bf4470b97a649997457992c2f80192a923503edaf144367391d3054c2ab73'
 default['windows_logrotate']['install_dir'] = "#{ENV['SYSTEMDRIVE']}\\logrotate"
 default['windows_logrotate']['zip_filename'] = "logrotateSetup_#{node['windows_logrotate']['version']}.zip"
-default['windows_logrotate']['url'] = 'https://pilotfiber.dl.sourceforge.net/project' \
-    "/logrotatewin/#{node['windows_logrotate']['zip_filename']}"
+default['windows_logrotate']['url'] = 'https://downloads.sourceforge.net/project' \
+    "/logrotatewin/#{node['windows_logrotate']['zip_filename']}?use_mirror=autoselect" \
