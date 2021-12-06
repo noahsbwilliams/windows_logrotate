6a5d07c2c3ec581dc8ec8ca9e2f7e116abb8264e
Noah Williams <nwilliams@zixcorp.com>
2021-12-06 08:59:34 -0900

    Update LogRotateWin to `0.0.0.18_20170228`

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['windows_logrotate']['version'] = '0.0.0.17_20170116'
-default['windows_logrotate']['sha256'] = 'd82efafdf085ef98a72dade1c7ac66c303f8fa902a7a80a5dceba3062e0f07fb'
+default['windows_logrotate']['version'] = '0.0.0.18_20170228'
+default['windows_logrotate']['sha256'] = '11ca344f67c60905935e3037021b2229d6eb76c629724bf2cbab43713e101fd7'
 default['windows_logrotate']['install_dir'] = "#{ENV['SYSTEMDRIVE']}\\logrotate"
 default['windows_logrotate']['zip_filename'] = "logrotateSetup_#{node['windows_logrotate']['version']}.zip"
 default['windows_logrotate']['url'] = 'https://pilotfiber.dl.sourceforge.net/project' \
